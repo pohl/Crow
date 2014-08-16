46b32af5a17be6d46cee86eecb5538695a8319de
Pohl Longsine <pohl.longsine@gmail.com>
2014-08-16 11:09:37 -0500

    Initial Commit

---
+++ Crow/Crow.h
@@ -0,0 +1,19 @@
+//
+//  Crow.h
+//  Crow
+//
+//  Created by Pohl Longsine on 8/16/14.
+//  Copyright (c) 2014 the screaming organization. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Crow.
+FOUNDATION_EXPORT double CrowVersionNumber;
+
+//! Project version string for Crow.
+FOUNDATION_EXPORT const unsigned char CrowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Crow/PublicHeader.h>
+
+
